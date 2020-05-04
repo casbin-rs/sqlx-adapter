4c7a7346443c212feefc1a6075d99f26558f6eb5
Hackerchai <hackerchai.com@gmail.com>
2020-05-04 11:44:06 +0800

    Feat: change CI to github actions

--- src/actions.rs
+++ src/actions.rs
@@ -496,9 +496,9 @@ pub(crate) async fn save_policy<'a>(
 }
 
 #[cfg(feature = "postgres")]
-pub(crate) async fn add_policy<'a>(
+pub(crate) async fn add_policy(
     mut conn: &ConnectionPool,
-    rule: NewCasbinRule<'a>,
+    rule: NewCasbinRule<'_>,
 ) -> Result<bool> {
     sqlx::query!(
         "INSERT INTO casbin_rules ( ptype, v0, v1, v2, v3, v4, v5 )
@@ -520,9 +520,9 @@ pub(crate) async fn add_policy<'a>(
 }
 
 #[cfg(feature = "mysql")]
-pub(crate) async fn add_policy<'a>(
+pub(crate) async fn add_policy(
     mut conn: &ConnectionPool,
-    rule: NewCasbinRule<'a>,
+    rule: NewCasbinRule<'_>,
 ) -> Result<bool> {
     sqlx::query!(
         "INSERT INTO casbin_rules ( ptype, v0, v1, v2, v3, v4, v5 )
@@ -544,9 +544,9 @@ pub(crate) async fn add_policy<'a>(
 }
 
 #[cfg(feature = "postgres")]
-pub(crate) async fn add_policies<'a>(
+pub(crate) async fn add_policies(
     conn: &ConnectionPool,
-    rules: Vec<NewCasbinRule<'a>>,
+    rules: Vec<NewCasbinRule<'_>>,
 ) -> Result<bool> {
     let mut transaction = conn
         .begin()
@@ -583,9 +583,9 @@ pub(crate) async fn add_policies<'a>(
 }
 
 #[cfg(feature = "mysql")]
-pub(crate) async fn add_policies<'a>(
+pub(crate) async fn add_policies(
     conn: &ConnectionPool,
-    rules: Vec<NewCasbinRule<'a>>,
+    rules: Vec<NewCasbinRule<'_>>,
 ) -> Result<bool> {
     let mut transaction = conn
         .begin()
